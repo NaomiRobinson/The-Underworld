5277923d9e89ce092b0ed94e70e10d4d4aba3ed9
Naomi Robinson <Nao.Robinson7@gmail.com>
2023-06-27 07:38:44 -0300

    cambio de rutas

--- assets/scenes/Precarga.js
+++ assets/scenes/Precarga.js
@@ -7,34 +7,34 @@ export default class Precarga extends Phaser.Scene {
 
   preload() {
 
-    this.load.image("fondo", "./assets/images/fondo.png");
-    this.load.image("piedras1", "./assets/images/piedras1.png");
-    this.load.image("piedras2", "./assets/images/piedras2.png");
-    this.load.image("piedras3", "./assets/images/piedras3.png");
-    this.load.image("botonPlay", "./assets/images/play.png");
-    this.load.image("pelo", "./assets/images/chica.png");
-    this.load.image("ladrillos", "./assets/images/ladrillos.png");
-    this.load.image("titulo", "./assets/images/titulo.png");
-    this.load.image("botonAyuda", "./assets/images/ayuda.png");
-    this.load.image("rectangulo", "./assets/images/rectangulo.png");
+    this.load.image("fondo", "/assets/images/fondo.png");
+    this.load.image("piedras1", "/assets/images/piedras1.png");
+    this.load.image("piedras2", "/assets/images/piedras2.png");
+    this.load.image("piedras3", "/assets/images/piedras3.png");
+    this.load.image("botonPlay", "/assets/images/play.png");
+    this.load.image("pelo", "/assets/images/chica.png");
+    this.load.image("ladrillos", "/assets/images/ladrillos.png");
+    this.load.image("titulo", "/assets/images/titulo.png");
+    this.load.image("botonAyuda", "/assets/images/ayuda.png");
+    this.load.image("rectangulo", "/assets/images/rectangulo.png");
     
-    this.load.image("plataforma", "../assets/images/plataforma.png");
-    this.load.image("jugador", "../assets/images/jugador.png");
-    this.load.image("ojo", "../assets/images/ojo.png");
-    this.load.image("fantasma", "../assets/images/fantasma.png")
-    this.load.image("fuego", "../assets/imagess/fuego.png");
-    this.load.image("moneda", "../assets/images/moneda.png");
-    this.load.image("caliz", "../assets/images/caliz.png");
-    this.load.image("collar", "../assets/images/collar.png");
-    this.load.image("gema", "../assets/images/gema.png");
-    this.load.image("espejo", "../assets/images/espejo.png");
+    this.load.image("plataforma", "/assets/images/plataforma.png");
+    this.load.image("jugador", "/assets/images/jugador.png");
+    this.load.image("ojo", "/assets/images/ojo.png");
+    this.load.image("fantasma", "/assets/images/fantasma.png")
+    this.load.image("fuego", "/assets/imagess/fuego.png");
+    this.load.image("moneda", "/assets/images/moneda.png");
+    this.load.image("caliz", "/assets/images/caliz.png");
+    this.load.image("collar", "/assets/images/collar.png");
+    this.load.image("gema", "/assets/images/gema.png");
+    this.load.image("espejo", "/assets/images/espejo.png");
 
-    this.load.image("botonreplay", "../assets/images/replay.png");
-    this.load.image("botonmenu", "../assets/images/menu.png");
+    this.load.image("botonreplay", "/assets/images/replay.png");
+    this.load.image("botonmenu", "/assets/images/menu.png");
 
-    this.load.image("vidaExtra", "../assets/images/heart.png");
+    this.load.image("vidaExtra", "/assets/images/heart.png");
 
-    this.load.spritesheet("personaje", "../assets/images/personaje.png", {
+    this.load.spritesheet("personaje", "/assets/images/personaje.png", {
       frameWidth: 43,
       frameHeight: 60,
     });
